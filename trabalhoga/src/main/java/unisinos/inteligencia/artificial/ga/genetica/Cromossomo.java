3f2ff00aa6e98a408f03946b207aed7df91628af
Alencar Rodrigo Hentges <alencarhentges@gmail.com>
2019-04-10 20:03:05 -0300

    Implementado cálculo de aptidão inicial
    Função somente considerando a distância total percorrida pelos caminhões

--- trabalhoga/src/main/java/unisinos/inteligencia/artificial/ga/genetica/Cromossomo.java
+++ trabalhoga/src/main/java/unisinos/inteligencia/artificial/ga/genetica/Cromossomo.java
@@ -1,17 +1,16 @@
 package unisinos.inteligencia.artificial.ga.genetica;
 
-import java.util.Map;
+import java.util.List;
 
 import lombok.Builder;
 import lombok.Data;
 import unisinos.inteligencia.artificial.ga.domain.Rota;
-import unisinos.inteligencia.artificial.ga.domain.Veiculo;
 
 @Data
 @Builder
 public class Cromossomo implements Comparable<Cromossomo> {
 
-    private Map<Veiculo, Rota> rotas;
+    private List<Rota> rotas;
     private Double aptidao;
 
     @Override
--- trabalhoga/src/main/java/unisinos/inteligencia/artificial/ga/genetica/funcoes/aptidao/FuncaoAptidao.java
+++ trabalhoga/src/main/java/unisinos/inteligencia/artificial/ga/genetica/funcoes/aptidao/FuncaoAptidao.java
@@ -1,14 +1,37 @@
 package unisinos.inteligencia.artificial.ga.genetica.funcoes.aptidao;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.util.List;
+
+import unisinos.inteligencia.artificial.ga.domain.Cidade;
+import unisinos.inteligencia.artificial.ga.domain.Rota;
 import unisinos.inteligencia.artificial.ga.genetica.Cromossomo;
 
 public class FuncaoAptidao {
 
     public double calcular(final Cromossomo cromossomo) {
-        //TODO deve atualizar valor da aptidao do cromossomo e retornar
-        cromossomo.setAptidao(0.0D);
-        throw new NotImplementedException();
+
+        double distanciaPercorridaTodosCaminhoes = cromossomo.getRotas().stream()
+            .map(Rota::getCaminho)
+            .map(this::calcularDistancia)
+            .mapToDouble(Double::doubleValue)
+            .sum();
+
+        cromossomo.setAptidao(distanciaPercorridaTodosCaminhoes);
+
+        return distanciaPercorridaTodosCaminhoes;
+    }
+
+    private Double calcularDistancia(final List<Cidade> caminho) {
+        double distanciaTotal = 0;
+
+        for (int i = 0; i < caminho.size() - 1; i++) {
+            final Cidade cidadeAtual = caminho.get(i);
+            final Cidade cidadeSeguinte = caminho.get(i + 1);
+
+            distanciaTotal += cidadeAtual.distanciaDe(cidadeSeguinte);
+        }
+
+        return distanciaTotal;
     }
 
 }
